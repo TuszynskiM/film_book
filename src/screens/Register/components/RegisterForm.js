ce15ffef95cd631f06b2fbf71ec58a9351eeb3f2
MIchciu <tuszynski.michal92@gmail.com>
2020-04-12 10:15:12 +0200

    Fix problem with empty localstorage

--- src/screens/Register/components/RegisterForm.js
+++ src/screens/Register/components/RegisterForm.js
@@ -9,110 +9,111 @@ import RegisterBtn from './RegisterBtn';
 import * as Yup from 'yup';
 
 const useStyles = makeStyles(() => ({
-  input: {
-    '& .MuiOutlinedInput-root': {
-      color: '#E60B0B',
-      height: '60px',
-      width: '400px',
-      '& fieldset': {
-        border: '2px solid #E60B0B',
-        borderRadius: '30px',
-        color: '#E60B0B'
-      },
-      '&:hover fieldset': {
-        borderColor: '#E60B0B',
-        color: '#E60B0B'
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: '#E60B0B',
-        color: '#E60B0B'
-      },
+    input: {
+        '& .MuiOutlinedInput-root': {
+            color: '#E60B0B',
+            height: '60px',
+            width: '400px',
+            '& fieldset': {
+                border: '2px solid #E60B0B',
+                borderRadius: '30px',
+                color: '#E60B0B'
+            },
+            '&:hover fieldset': {
+                borderColor: '#E60B0B',
+                color: '#E60B0B'
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#E60B0B',
+                color: '#E60B0B'
+            },
+        },
+        '& label': {
+            color: '#E60B0B',
+        },
+        '& label.Mui-focused': {
+            color: '#E60B0B',
+        },
+        marginTop: 20,
     },
-    '& label': {
-      color: '#E60B0B',
-    },
-    '& label.Mui-focused': {
-      color: '#E60B0B',
-    },
-    marginTop: 20,
-  },
-  form: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-  }
+    form: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    }
 }));
 
 const Schema = Yup.object().shape({
-  login: Yup.string()
-      .min(6, "Podany login jest za krotki, podaj minimum 6 znaków")
-      .required("Pole jest wymagane"),
-  email: Yup.string()
-      .email('Podano nie prawidłowy email')
-      .required("Pole jest wymagane"),
-  password: Yup.string()
-      .min(6, "Podane hasło jest za krotkie, podaj minimum 6 znaków")
-      .required("Pole jest wymagane")
+    login: Yup.string()
+        .min(6, "Podany login jest za krotki, podaj minimum 6 znaków")
+        .required("Pole jest wymagane"),
+    email: Yup.string()
+        .email('Podano nie prawidłowy email')
+        .required("Pole jest wymagane"),
+    password: Yup.string()
+        .min(6, "Podane hasło jest za krotkie, podaj minimum 6 znaków")
+        .required("Pole jest wymagane")
 })
 
 
 const RegisterForm = () => {
-  const classes = useStyles();
-  let history = useHistory();
+    const classes = useStyles();
+    let history = useHistory();
 
 
-  const handleSubmit = (value, {resetForm}) => {
-    let accounts = localStorage.getItem('accounts')
-    accounts = [JSON.parse(accounts), value]
-    localStorage.setItem('accounts', JSON.stringify(accounts));
-    history.push(ROUTE.LOGIN)
-    resetForm()
-  }
+    const handleSubmit = (value, {resetForm}) => {
+        let accounts = localStorage.getItem('accounts')
+        accounts = JSON.parse(accounts);
+        accounts = accounts ? [accounts, value] : [value]
+        localStorage.setItem('accounts', JSON.stringify(accounts));
+        history.push(ROUTE.LOGIN)
+        resetForm()
+    }
 
-  return (
-      <Formik
-          initialValues={{
-            login: '',
-            email: '',
-            password: ''
-          }}
-          validationSchema={Schema}
-          onSubmit={handleSubmit}
-      >
-        <Form>
-          <Box className={classes.form}>
-            <Field
-                component={TextField}
-                name='login'
-                type='text'
-                label='Login'
-                variant="outlined"
-                className={classes.input}
-                autoComplete='off'
-            />
-            <Field
-                component={TextField}
-                name='password'
-                type='password'
-                label='Hasło'
-                variant="outlined"
-                className={classes.input}
-            />
-            <Field
-                component={TextField}
-                name='email'
-                type='email'
-                label='Email'
-                variant="outlined"
-                className={classes.input}
-            />
-          </Box>
-          <RegisterBtn/>
+    return (
+        <Formik
+            initialValues={{
+                login: '',
+                email: '',
+                password: ''
+            }}
+            validationSchema={Schema}
+            onSubmit={handleSubmit}
+        >
+            <Form>
+                <Box className={classes.form}>
+                    <Field
+                        component={TextField}
+                        name='login'
+                        type='text'
+                        label='Login'
+                        variant="outlined"
+                        className={classes.input}
+                        autoComplete='off'
+                    />
+                    <Field
+                        component={TextField}
+                        name='password'
+                        type='password'
+                        label='Hasło'
+                        variant="outlined"
+                        className={classes.input}
+                    />
+                    <Field
+                        component={TextField}
+                        name='email'
+                        type='email'
+                        label='Email'
+                        variant="outlined"
+                        className={classes.input}
+                    />
+                </Box>
+                <RegisterBtn/>
 
-        </Form>
-      </Formik>
-  )
+            </Form>
+        </Formik>
+    )
 };
 
 export default RegisterForm;
