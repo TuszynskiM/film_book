75cbeb57c46d71b4f05e164e28cfef48683bcb59
MIchciu <tuszynski.michal92@gmail.com>
2020-05-09 12:13:13 +0200

    fix remove account

--- src/screens/Main/components/SettingModal/components/DeleteAccount/DeleteAccountForm.js
+++ src/screens/Main/components/SettingModal/components/DeleteAccount/DeleteAccountForm.js
@@ -1,55 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as Yup from 'yup';
 import CustomForm from '../../../../../shared/components/CustomForm';
 import CustomBtn from '../../../../../shared/components/CustomBtn';
 import {deleteUser} from "../../../../../../api/deleteUser";
+import ErrorMassage from "../../../../../shared/components/ErrorMassage";
+import {ROUTE} from "../../../../../../App/route-config";
+import {useHistory} from 'react-router-dom';
 
 const Schema = Yup.object().shape({
-  login: Yup.string()
-      .min(6, "Podany login jest za krotki, podaj minimum 6 znaków")
-      .required("Pole jest wymagane"),
-  password: Yup.string()
-      .min(6, "Podane hasło jest za krotkie, podaj minimum 6 znaków")
-      .required("Pole jest wymagane")
+    login: Yup.string()
+        .min(6, "Podany login jest za krotki, podaj minimum 6 znaków")
+        .required("Pole jest wymagane"),
+    password: Yup.string()
+        .min(6, "Podane hasło jest za krotkie, podaj minimum 6 znaków")
+        .required("Pole jest wymagane")
 })
 const initialValues = {
-  login: '',
-  password: ''
+    login: '',
+    password: ''
 }
 const inputConfig = [
-  {
-    name: 'login',
-    type: 'text',
-    label: 'Login'
-  },
-  {
-    name: 'password',
-    type: 'password',
-    label: 'Hasło'
-  }
+    {
+        name: 'login',
+        type: 'text',
+        label: 'Login'
+    },
+    {
+        name: 'password',
+        type: 'password',
+        label: 'Hasło'
+    }
 ]
 
 const DeleteAccountForm = () => {
-  const btnText = 'Usuń konto';
+    const btnText = 'Usuń konto';
+    const [mesg, setMesg] = useState('');
+    const history = useHistory();
+
+    const handleSubmit = (value, {resetForm}) => {
+        console.log(value)
+        const user = deleteUser(value.login, value.password)
+
+        user.then(resp => {
+            setMesg(resp.message)
+            if (resp.message === 'Konto usunięte poprawnie') {
+                setTimeout(() => {
+                    localStorage.removeItem('logged')
+                    localStorage.removeItem('loggedId')
+                    history.push(ROUTE.LOGIN)
+                }, 5000)
+            }
+        })
+        resetForm();
+    }
 
 
-  const handleSubmit =(value, )=>{
-    console.log(value)
-    const user = deleteUser(value.login, value.password)
-
-    user.then(resp=> {
-      console.log(resp.message)
-    })
-  /*  localStorage.removeItem('logged')
-    history.push(ROUTE.LOGIN)*/
-  }
-
-
-  return (
-      <CustomForm inputConfig={inputConfig} initialValue={initialValues} validationSchema={Schema} handleSubmit={handleSubmit}>
-        <CustomBtn btnText={btnText}/>
-      </CustomForm>
-  )
+    return (
+        <CustomForm inputConfig={inputConfig} initialValue={initialValues} validationSchema={Schema}
+                    handleSubmit={handleSubmit}>
+            <ErrorMassage>
+                {mesg}
+            </ErrorMassage>
+            <CustomBtn btnText={btnText}/>
+        </CustomForm>
+    )
 };
 
 export default DeleteAccountForm;
